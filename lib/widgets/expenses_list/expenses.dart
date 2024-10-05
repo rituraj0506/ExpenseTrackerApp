44ea608b3117245ab891b1f71cd13258ae1d44b8
rituraj0506 <riturajkumar7256@gmail.com>
2024-10-05 22:45:35 +0530

    5 oct

--- lib/widgets/expenses_list/expenses.dart
+++ lib/widgets/expenses_list/expenses.dart
@@ -26,8 +26,16 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => NewExpense(),
+      builder: (ctx) => NewExpense(
+        onAddExpense: _addExpense,
+      ),
     );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
   }
 
   @override
--- lib/widgets/expenses_list/new_expense.dart
+++ lib/widgets/expenses_list/new_expense.dart
@@ -1,26 +1,94 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:expensetracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() => _NewExpenseState();
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  // var _entertitle = '';
-  // void _saveTitleInput(String inputValue) {
-  //   _entertitle = inputValue;
-  // }
-
   var _titlecontroller = TextEditingController();
   var _amountcontroller = TextEditingController();
+  var _datecontroller = TextEditingController();
 
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 100, now.month, now.day);
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+    // if (pickedDate != null) {
+    //   setState(() {
+    //     _selectedDate = pickedDate;
+    //     // Format the date and set it in the date controller
+    //     _datecontroller.text =
+    //         "${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}";
+    //   });
+    // }
+
+    //  Expanded(
+    //                   child: TextField(
+    //                     controller: _datecontroller, // Attach the controller
+    //                     decoration: InputDecoration(
+    //                       hintText: 'Select a date',
+    //                     ),
+    //                     readOnly: true, // Makes the field non-editable
+    //                   ),
+    //                 ),
+  }
+
+  void _submitexpensedata() {
+    final enteredamount = double.tryParse(_amountcontroller.text);
+    final amountisvalid = enteredamount == null || enteredamount <= 0;
+    if (_titlecontroller.text.trim().isEmpty ||
+        amountisvalid ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: Text("Invalid Input"),
+          content: Text("Please Enter Valid Data"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: Text("Okay"),
+            ),
+          ],
+        ),
+      );
+      return;
+    }
+    widget.onAddExpense(
+      Expense(
+          title: _titlecontroller.text,
+          amount: enteredamount,
+          date: _selectedDate!,
+          category: _selectedCategory),
+    );
+  }
+
+  @override
   void dispose() {
     _titlecontroller.dispose();
     _amountcontroller.dispose();
+    _datecontroller.dispose();
     super.dispose();
   }
 
@@ -53,10 +121,15 @@ class _NewExpenseState extends State<NewExpense> {
               Expanded(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text("Selected Date"),
+                    Text(
+                      _selectedDate == null
+                          ? 'No date Selected'
+                          : formatter.format(_selectedDate!),
+                    ),
                     IconButton(
-                      onPressed: () {},
+                      onPressed: _presentDatePicker,
                       icon: Icon(Icons.calendar_month),
                     ),
                   ],
@@ -64,8 +137,28 @@ class _NewExpenseState extends State<NewExpense> {
               ),
             ],
           ),
+          const SizedBox(height: 16),
           Row(
             children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map(
+                        (category) => DropdownMenuItem(
+                          value: category,
+                          child: Text(
+                            category.name.toString(),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    if (value == null) return;
+                    setState(() {
+                      _selectedCategory = value;
+                    });
+                  }),
+              Spacer(),
               TextButton(
                 onPressed: () {
                   Navigator.pop(context);
@@ -73,10 +166,7 @@ class _NewExpenseState extends State<NewExpense> {
                 child: Text("Cancel"),
               ),
               ElevatedButton(
-                onPressed: () {
-                  print(_titlecontroller.text);
-                  print(_amountcontroller.text);
-                },
+                onPressed: _submitexpensedata,
                 child: Text("saveExpense"),
               ),
             ],
